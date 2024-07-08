c2354b44a58140e77e66d2458917572263fd3fba
Miki <muneshmyke@gmail.com>
2024-07-08 16:46:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const pool = new Pool({
 });
 
 app.use(cors()); // Enable CORS for all routes
-
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -33,12 +32,10 @@ app.get('/users', async (req, res) => {
   }
 });
 
-// Example route with CORS enabled
-app.get('/getJournal', async (req, res) => {
+// Endpoint to insert a new journal entry
+app.post('/journal', async (req, res) => {
+  const { title, content, category, date } = req.body;
   try {
-
-   
-
     const result = await pool.query(
       'INSERT INTO journal (title, content, category, date) VALUES ($1, $2, $3, $4) RETURNING *',
       [title, content, category, date]
@@ -49,8 +46,21 @@ app.get('/getJournal', async (req, res) => {
     res.status(500).send('Server error');
   }
 });
+
+// Endpoint to fetch all journal entries
+app.get('/journal', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM journal ORDER BY date DESC');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
+
+// Endpoint to insert a new category
 app.post('/category', async (req, res) => {
-  const {  category } = req.body;
+  const { category } = req.body;
   try {
     const result = await pool.query(
       'INSERT INTO category (category) VALUES ($1) RETURNING *',
@@ -63,19 +73,8 @@ app.post('/category', async (req, res) => {
   }
 });
 
-// Endpoint to fetch all journal entries
-app.get('/getJournal', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT * FROM journal ORDER BY date DESC');
-
-    res.json(result.rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server error');
-  }
-});
-
-app.get('/getCategory', async (req, res) => {
+// Endpoint to fetch all categories
+app.get('/category', async (req, res) => {
   try {
     const result = await pool.query('SELECT * FROM category');
     res.json(result.rows);
@@ -88,4 +87,3 @@ app.get('/getCategory', async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
